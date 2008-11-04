b1437e44c0937b196ec47c9806c6dbf09fa1fd85
Cary Zhang <melcloud2005@gmail.com>
2008-11-04 20:35:44 +1100

    modify user model to reflect the change with message system

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,9 +17,13 @@ class User < ActiveRecord::Base
                               :geometry => "72x72>",
                               :versions => {"small" => "16x16", "medium" => "32x32", "large" => "48x48"}
                             }
-  
+	#add relationship between messages and folders			
+  has_many :sent_messages, :class_name => "Message", :foreign_key => "author_id"
+	has_many :received_messages, :class_name => "MessageCopy", :foreign_key => "recipient_id"
+	has_many :folders
+
   belongs_to :geo
-  
+	  
   before_save :encrypt_password
   
   # prevents a user from submitting a crafted form that bypasses activation

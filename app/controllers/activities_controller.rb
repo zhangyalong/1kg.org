1253f2fa1c8f45fadc41106775601cd7c95dd6fe
Suave Su <suave.su@gmail.com>
2008-11-29 16:25:40 +0800

    remove repeat code for geo select at submit activity page, done activity info display, display format need to refactor

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -16,26 +16,9 @@ class ActivitiesController < ApplicationController
     flash[:notice] = "发布成功"
     redirect_to activities_url
   end
-  
-  def geo_choice
-    if !params[:departure_root_id].blank?
-      @root = Geo.find(params[:departure_root_id])
-      if @root.children.size > 0
-        @geos = @root.children
-      else
-        @geos = [ @root ]
-      end
-      render :partial => "geo_selector", :locals => {:dom_id => "departure_id"}
-      
-    elsif !params[:arrival_root_id].blank?
-      @root = Geo.find(params[:arrival_root_id])
-      if @root.children.size > 0
-        @geos = @root.children
-      else
-        @geos = [ @root ]
-      end
-      render :partial => "geo_selector", :locals => {:dom_id => "arrival_id"}
-    end
+
+  def show
+    @activity = Activity.find(params[:id])
   end
   
 end
